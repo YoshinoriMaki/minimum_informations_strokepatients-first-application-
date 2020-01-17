2fc5205d20decc4189e6c1e62cad420ce4acdb55
YoshinoriMaki <59092156+YoshinoriMaki@users.noreply.github.com>
2020-01-17 19:05:53 +0900

    user controller created

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,2 +1,24 @@
 class InformationsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def show
+  end
+  
 end
